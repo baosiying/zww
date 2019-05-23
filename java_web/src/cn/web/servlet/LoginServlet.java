44bd94edec0420d602260121c6f9d325523b6294
baosiying <baosiying@LAPTOP-7UN5FN4L>
2019-05-23 23:13:41 +0800

    鲍思颖修改

--- java_web/src/cn/web/servlet/LoginServlet.java
+++ java_web/src/cn/web/servlet/LoginServlet.java
@@ -57,32 +57,32 @@ public class LoginServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("------");
-		    //拿到客户输入的验证码
+		    //验证码
 		    String clientCode=request.getParameter("clientCode");
 		    System.out.println(clientCode);
-		   //拿到session中存的验证码
+		   //验证码图片中的内容
 		    String code=request.getSession().getAttribute("code").toString();
 		    System.out.println(code);
-		    //拿到权限按钮中的值(默认勾选值为1是管理员,为空就是用户或作者)
+		    //权限按钮
 		    String quanxian=request.getParameter("quanxian");
 		    System.out.println(quanxian);
-		    //拿到客户输入的用户名
+		    //用户名
 		     String uname=request.getParameter("uname");
-		     //拿到客户输入的密码
+		     //密码
 		     String upassword=request.getParameter("upassword");
 		     UsersService service=new UsersServiceImpl();
 		     Users u=null;
-		     //根据用户名进行查询此人是否存在
+		     //通过用户查询密码
 		     try {
 				    u=service.selectUsersByUname(uname);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		    //权限按钮中的值为空进入用户界面,否则进入管理员界面
-		    if(code.equals(clientCode)&&quanxian==null&&u!=null){
-		    	    //将用户名存入session中
+		     String pass=u.getUpassword();
+		    //与数据库比较
+		    if(code.equals(clientCode)&&quanxian==null&&u!=null&&pass.equals(upassword)){
 		    	    request.getSession().setAttribute("uname", uname);
-		    	    request.getRequestDispatcher("index.jsp").forward(request,response);
+		    	    request.getRequestDispatcher("usermain.jsp").forward(request,response);
 		    }else{
 		    	    response.sendRedirect("login.jsp");
 		    }
